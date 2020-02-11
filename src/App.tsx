51ffeaf08680442a1573da33764765dee3731139
coderq <yongqiang719@gmail.com>
2020-02-11 20:30:03 +0800

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Test from 'src/pages/app';
+import Home from 'src/pages/home';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Test />
+        </Route>
+      </Switch>
+    </Router>
+  } 
 }
-React.Component
+
 export default App;
